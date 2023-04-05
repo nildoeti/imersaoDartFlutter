079a88c75da272b79593f354f1bcd530bd70307b
Nildo <nildo@webstack.com.br>
2023-04-05 09:02:26 -0400

    finalizado exemplo para tipos de dados em dart

---
+++ introducao/variaveis.dart
@@ -0,0 +1,20 @@
+void main() {
+  // tipo inteiro
+  int idade = 900;
+
+  // tipo real
+  double polegada = 7.98;
+
+  // tipo string
+  String fruta = 'Banana';
+
+  // tipo lógico
+  bool frio = true;
+  bool quente = false;
+
+  print(idade);
+  print(polegada);
+  print(fruta);
+  print(frio);
+  print(quente);
+}
