5106ff2ec59a1d949fe3b14c534ad1247dcfa4d2
Nildo <nildo@webstack.com.br>
2023-04-06 19:18:27 -0300

    finalizado exemplo para funções do projeto

---
+++ funcoes/funcoes.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  // chamndo a função saudacoes
+  saudacoes();
+}
+
+// exemplo funcoes
+void saudacoes() {
+  print('olá Mundo!');
+  print('Estou estudando Dart com Flutter.');
+}
