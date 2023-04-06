bbdc1d65ed6967f325638865e4c42b06b117fef0
Nildo <nildo@webstack.com.br>
2023-04-06 18:33:22 -0300

    finalizado exemplo para do-while com dart

---
+++ introducao/do-while.dart
@@ -0,0 +1,21 @@
+// exemplo 2
+void main() {
+  int i = 0;
+
+  do {
+    if (i % 2 == 1) {
+      print(i);
+    }
+    i++;
+  } while (i <= 10);
+}
+
+// exemplo 1
+// void main() {
+//   int number = 0;
+
+//   do {
+//     number++;
+//     print(number);
+//   } while (number < 5);
+// }
