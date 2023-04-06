7c5935a967a1a195502b85abb56475993d4891e8
Nildo <nildo@webstack.com.br>
2023-04-06 17:12:36 -0400

    finalizado exemplo para while em dart

---
+++ introducao/while.dart
@@ -0,0 +1,65 @@
+// exemplo 6 - números impares até 10
+void main() {
+  int i = 0;
+  while (i < 20) {
+    if (i % 2 == 0) {
+      print(i + 1);
+    }
+    i++;
+  }
+}
+
+// exemplo 5 - números pares até 10
+// void main() {
+//   int i = 1;
+//   while (i < 10) {
+//     if (i % 2 == 0) {
+//       print(i);
+//     }
+//     i++;
+//   }
+// }
+
+// exemplo 4
+// void main() {
+//   int current = 0;
+//   while (current < 5) {
+//     current++;
+//     print(current);
+//   }
+//   print('Fim...');
+// }
+
+// // exemplo 3
+// void main() {
+//   int i = 0;
+//   int j = 5;
+//   while (i <= 10 && j >= 0) {
+//     print('$i - $j');
+//     print(j);
+
+//     i++;
+//     j--;
+//   }
+// }
+
+// exemplo 2
+// void main() {
+//   int i = 10;
+//   while (i > 0) {
+//     print(i);
+//     i--;
+//   }
+//   print('Fim...');
+// }
+
+// // exemplo 1
+// void main() {
+//   int i = 0;
+//   while (i <= 100) {
+//     print(i);
+//     i++;
+//   }
+
+//   print('Fim...');
+// }
