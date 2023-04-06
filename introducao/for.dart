fa59b7e275c903750c05b613345eeb7df4f2a71f
Nildo <nildo@webstack.com.br>
2023-04-06 19:05:57 -0300

    finalizado exemplo para for com dart

---
+++ introducao/for.dart
@@ -0,0 +1,37 @@
+// exemplo 5
+void main(List<String> args) {
+  for (var i = 0; i <= 5; i++) {
+    for (int j = 5; j > 0; j--) {
+      print('$i');
+      print('$j');
+    }
+  }
+}
+
+// exemplo 4
+// void main(List<String> args) {
+//   for (int i = 10; i >= 0; i--) {
+//     print('i : $i');
+//   }
+// }
+
+// // exemplo 3
+// void main(List<String> args) {
+//   for (int i = 10; i >= 0; i--) {
+//     print(i);
+//   }
+// }
+
+// exemplo 2
+// void main(List<String> args) {
+//   for (int i = 0; i < 10; i += 2) {
+//     print(i);
+//   }
+// }
+
+// exemplo 1
+// void main(List<String> args) {
+//   for (int i = 0; i < 5; i++) {
+//     print(i);
+//   }
+// }
