822e95e32468ee4e4c111cd3e9776e765d3c5bc2
Nildo <nildo@webstack.com.br>
2023-04-07 08:14:55 -0300

    finalizado exemplo para retorno da função emd art

---
+++ funcoes/retorno-da-funcao.dart
@@ -0,0 +1,14 @@
+void main() {
+  saudacao();
+}
+
+void saudacao() {
+  print('Olá Mundo!');
+  print('Estou estudando Dart com Futter.');
+  print('${agora()}');
+}
+
+String agora() {
+  DateTime agora = DateTime.now();
+  return agora.toString();
+}
