290f450e5da6936ab700fe571a75ca1cea556e2b
Nildo <nildo@webstack.com.br>
2023-05-06 11:46:31 -0300

    finalizado exemplos para mapas

---
+++ colecoes/mapas.dart
@@ -0,0 +1,28 @@
+void main(List<String> args) {
+  Map<int, String?> dds = {
+    11: 'São Paulo',
+    21: 'Rio de Janeiro',
+    31: null,
+    41: 'Não definido'
+  };
+
+  print(dds);
+  String? cidade = dds[11]; // variável cidade recebe dd 11 e cidade são paulo
+  print(cidade);
+
+  dds[61] = 'Brasilia';
+  print(dds);
+  
+  dds.remove(31);
+  print(dds);
+
+  print(dds.values); // retorna os valores das chaves
+  print(dds.keys); // retorna as chaves 
+  print(dds.containsKey(11));
+  print(dds.containsKey(99));
+  print(dds.containsValue('São Paulo'));
+  print(dds.containsValue('Campinas'));
+  print(dds.isEmpty); // esta a lista vazio?
+  print(dds.isNotEmpty); // a lista não esta vazia?
+
+}
